7549c61087fa5af71c99f405491658ee24ba962a
javiercolodro <javiercolodro@gmail.com>
2024-09-20 23:05:53 -0300

    add frontend render url

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,8 @@ import { setUser } from "./redux/user";
 import ForgotPassword from "./components/ForgotPassword";
 import RepeatPassword from "./components/RepeatPassword";
 
+const URL_FRON_RENDER = import.meta.env.FRONTEND_URL;
+
 function App() {
   const dispatch = useDispatch();
 
@@ -46,7 +48,7 @@ function App() {
             <Route path="/video-form/:id" element={<VideoForm />} />
             <Route path="forgot-password" element={<ForgotPassword />} />
             <Route
-              path="/repeat-password/:token"
+              path={`${URL_FRON_RENDER}/repeat-password/:token`}
               element={<RepeatPassword />}
             />
           </Routes>
